e9f7fcd6cf9ec5e0b8046e51d8e6b4db367a6e52
Stealth <67294868+ImNotStealth@users.noreply.github.com>
2021-09-11 01:44:37 +0200

    Prototype of flexbox working

--- public/js/home.js
+++ public/js/home.js
@@ -2,25 +2,18 @@ console.log("BOI");
 $.getJSON("/api/video").done((data) => {
     console.log(data);
     for (const i in data) {
-        const cosmetic = document.createElement("button");
+        const video = document.createElement("button");
         const img = document.createElement("img");
-        const titleBackground = document.createElement("div");
-        const title = document.createElement("p");
-        cosmetic.classList.add("cosmetic");
-        img.src = data[i].icon;
-        img.classList.add("cosmetic-img");
-        titleBackground.classList.add("cosmetic-title-background");
-        title.innerHTML = data[i].displayName;
-        title.classList.add("cosmetic-title");
-        titleBackground.appendChild(title);
-        cosmetic.appendChild(img);
-        cosmetic.appendChild(titleBackground);
-        cosmetic.setAttribute("uuid", data[i].id);
-        document.getElementsByClassName("cosmetic-grid")[0].appendChild(cosmetic);
+        video.classList.add("video");
+        img.src = data[i].thumbnail;
+        img.classList.add("video-img");
+        video.appendChild(img);
+        video.setAttribute("v-id", data[i].id);
+        document.getElementsByClassName("container")[0].appendChild(video);
 
-        cosmetic.addEventListener("click", (e) => {
+        video.addEventListener("click", (e) => {
           console.log("CLICK " + data[i].id);
-          location.href = `/cosmetics/manage/${data[i].id}`;
+          location.href = `/watch/${data[i].id}`;
         })
     }
 })
